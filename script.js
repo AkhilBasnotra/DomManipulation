53086b1fc7efe93a511454caa10a9f9074008f46
Akhil Basnotra <akhilbasnotra24@gmail.com>
2023-10-24 23:35:01 +0530

    assignment3

---
+++ script.js
@@ -0,0 +1,28 @@
+// console.dir(document)
+
+// let headerTitle =  document.getElementById('header-title')
+// headerTitle.style.border = 'solid 3px black'
+
+
+// var addItemsTitle = document.querySelector('.title:contains("Add Items")');
+
+//   // Check if the element is found
+//   if (addItemsTitle) {
+//     // Update the style properties
+//     addItemsTitle.style.fontWeight = 'bold';
+//     addItemsTitle.style.color = 'green';
+//   }
+
+
+let headerTitle = document.getElementById('header-title')
+let mainHeader = document.getElementById('main-header')
+let title = document.getElementsByClassName('title')
+
+// console.log(headerTitle)
+
+// headerTitle.textContent = 'happy'
+// headerTitle.innerText = 'sad'
+
+headerTitle.style.border = 'solid 3px black' 
+title[0].style.color = 'green'
+title[0].style.fontWeight = 'bold'
