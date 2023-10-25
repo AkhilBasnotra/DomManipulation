a55e3abb46adf8c2ab017cc3ab9a3a69ee199e0b
Akhil Basnotra <akhilbasnotra24@gmail.com>
2023-10-25 13:57:01 +0530

    added assignment 6

---
+++ assignment6/script.js
@@ -0,0 +1,6 @@
+let secondItem = document.querySelector('.list-group-item:nth-child(2)')
+let thirdItem = document.querySelector('.list-group-item:nth-child(3)')
+
+
+secondItem.style.backgroundColor = 'green'
+thirdItem.style.visibility = 'hidden'
