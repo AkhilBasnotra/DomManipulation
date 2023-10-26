bcb84c529c552878616b1c7ac9504c99d42ab0b5
Akhil Basnotra <akhilbasnotra24@gmail.com>
2023-10-26 18:29:54 +0530

    added assignment7

---
+++ assignment7/script.js
@@ -0,0 +1,69 @@
+let itemList = document.querySelector('#items')
+
+// 1. parentnode
+console.log(itemList.parentNode)
+itemList.parentNode.style.backgroundColor = '#f4f4f4'
+
+// 2. childNodes
+console.log(itemList.childNodes)
+
+// 3. childern
+console.log(itemList.children)
+console.log(itemList.children[1])
+itemList.children[1].style.backgroundColor = 'yellow'
+
+// 4. firstChild
+console.log(itemList.firstChild)
+
+// 5. firstElementChild
+console.log(itemList.firstElementChild)
+itemList.firstElementChild.textContent = 'hello1'
+console.log(itemList.firstElementChild.textContent)
+
+// 6. lastChild
+console.log(itemList.lastChild)
+
+// 7. lastElementChild
+console.log(itemList.lastElementChild)
+itemList.lastElementChild.textContent = 'hello2'
+
+// 8. nextSibling
+console.log(itemList.nextSibling)
+
+// 9. nextElementSibling
+console.log(itemList.nextElementSibling)
+
+// 10. previousSibling
+console.log(itemList.previousSibling)
+
+// 11. previousElementSibling
+console.log(itemList.previousElementSibling)
+itemList.previousElementSibling.style.color = 'green'
+
+// 12. createElement
+let newDiv = document.createElement('div')
+newDiv.className = 'hello' //add class//
+newDiv.id = 'hello1' //add id//
+newDiv.setAttribute('title', 'hello div')//add title//
+
+let text = document.createTextNode('hello')//create next node//
+newDiv.appendChild(text)//add text to div//
+
+//adding elements to container
+let container = document.querySelector('header .container')
+let h1 = document.querySelector('header h1')
+
+console.log(newDiv)
+
+newDiv.style.fontSize = '30px'
+container.insertBefore(newDiv, h1)
+
+
+
+
+
+//adding HELLo word before item 1
+
+let parentnode = document.getElementById('items')
+parentnode.innerHTML = '<li>Hello</li>' + parentnode.innerHTML
+// console.log(parentnode.innerHTML)
