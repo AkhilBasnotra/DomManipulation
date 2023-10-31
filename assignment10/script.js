6a0ff9b6c18ae080ba5909b3c049115092862449
Akhil Basnotra <akhilbasnotra24@gmail.com>
2023-10-31 14:01:02 +0530

    corrected assignment 10

--- assignment10/script.js
+++ assignment10/script.js
@@ -1,27 +1,29 @@
-// let myForm = document.getElementById('my-form')
-// let nameInput = document.getElementById('name')
-// let emailInput = document.querySelector('#email')
-// let userList = document.getElementById('users')
+let myForm = document.getElementById('my-form')
+let nameInput = document.getElementById('name')
+let emailInput = document.querySelector('#email')
+let userList = document.getElementById('users')
 
-// myForm.addEventListener('submit', onSubmit)
+myForm.addEventListener('submit', onSubmit)
 
-// function onSubmit(e){
-//     e.preventDefault()
+function onSubmit(e){
+    e.preventDefault()
+
+    let name = nameInput.value
+    let email = emailInput.value
+
+    localStorage.setItem('name', name)
+    localStorage.setItem('email', email)
     
     
-//     let li = document.createElement('li')
-//     li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`))
+    let li = document.createElement('li')
+    li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`))
 
-//     userList.appendChild(li)
+    userList.appendChild(li)
     
-//     nameInput.value = ''
-//     emailInput.value= ''
+    nameInput.value = ''
+    emailInput.value= ''
     
 
-//     let name = nameInput.value
-//     let email = emailInput.value
+    
 
-//     localStorage.setItem('name', name)
-//     localStorage.setItem('email', email)
-
-// }
+}
