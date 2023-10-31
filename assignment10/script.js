158af93f75c423416e8e5ecbfba7890bd24e7cef
Akhil Basnotra <akhilbasnotra24@gmail.com>
2023-10-31 13:53:10 +0530

    added assignment 10

---
+++ assignment10/script.js
@@ -0,0 +1,27 @@
+// let myForm = document.getElementById('my-form')
+// let nameInput = document.getElementById('name')
+// let emailInput = document.querySelector('#email')
+// let userList = document.getElementById('users')
+
+// myForm.addEventListener('submit', onSubmit)
+
+// function onSubmit(e){
+//     e.preventDefault()
+    
+    
+//     let li = document.createElement('li')
+//     li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`))
+
+//     userList.appendChild(li)
+    
+//     nameInput.value = ''
+//     emailInput.value= ''
+    
+
+//     let name = nameInput.value
+//     let email = emailInput.value
+
+//     localStorage.setItem('name', name)
+//     localStorage.setItem('email', email)
+
+// }
