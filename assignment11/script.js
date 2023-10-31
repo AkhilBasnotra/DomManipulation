5f71dfcdd7b97492e2f88162ad039219fe6381a8
Akhil Basnotra <akhilbasnotra24@gmail.com>
2023-10-31 20:41:50 +0530

    added assignment 11

---
+++ assignment11/script.js
@@ -0,0 +1,31 @@
+let myForm = document.getElementById('my-form')
+let nameInput = document.getElementById('name')
+let emailInput = document.querySelector('#email')
+let userList = document.getElementById('users')
+
+myForm.addEventListener('submit', onSubmit)
+
+function onSubmit(e){
+    e.preventDefault()
+
+    let newUser = {
+         name : nameInput.value,
+         email : emailInput.value,
+    }
+
+    let newUser_serializer = JSON.stringify(newUser)
+    
+
+    localStorage.setItem('userDetails', newUser_serializer)
+    
+
+    let li = document.createElement('li')
+    li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`))
+
+    userList.appendChild(li)
+    
+    nameInput.value = ''
+    emailInput.value= ''
+    
+
+}
